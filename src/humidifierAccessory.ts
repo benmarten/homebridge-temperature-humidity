a67ee2350b97edaf6b5b715f32058054a82c4a52
Ben Marten <ben.marten@me.com>
2024-03-16 14:26:14 -0700

    add humidifier switch

---
+++ src/humidifierAccessory.ts
@@ -0,0 +1,89 @@
+import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge'
+import { ClimateHomebridgePlatform } from './platform'
+import { request } from 'http'
+import { HUMIDIFIER_IP, HUMIDIFIER_PORT } from './settings'
+
+export class HumidifierAccessory {
+  private service: Service
+  private states = { on: false }
+
+  constructor(
+    private readonly platform: ClimateHomebridgePlatform,
+    private readonly accessory: PlatformAccessory
+  ) {
+    this.accessory
+      .getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(
+        this.platform.Characteristic.Manufacturer,
+        'Default-Manufacturer'
+      )
+      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
+      .setCharacteristic(
+        this.platform.Characteristic.SerialNumber,
+        'Default-Serial'
+      )
+
+    // Humidifer Service
+    this.service =
+      this.accessory.getService(this.platform.Service.Switch) ||
+      this.accessory.addService(this.platform.Service.Switch)
+    this.service.setCharacteristic(
+      this.platform.Characteristic.Name,
+      'Humidifier'
+    )
+    this.service
+      .getCharacteristic(this.platform.Characteristic.On)
+      .onSet(this.setOn.bind(this))
+      .onGet(this.getOn.bind(this))
+  }
+
+  async setOn(value: CharacteristicValue) {
+    this.states.on = value as boolean
+
+    const req = request(
+      {
+        hostname: HUMIDIFIER_IP,
+        port: HUMIDIFIER_PORT,
+        path: `/switch/kauf_plug/turn_${this.states.on ? 'on' : 'off'}`,
+        method: 'POST',
+      },
+      (res) => res.on('data', (d) => this.platform.log.debug(d))
+    )
+    req.on('error', (error) =>
+      this.platform.log.error(
+        error.name + '\n' + error.message + '\n' + error.stack
+      )
+    )
+    req.end()
+
+    this.platform.log.debug('Humidifier On ->', value)
+  }
+
+  async getOn(): Promise<CharacteristicValue> {
+    return new Promise((resolve, reject) => {
+      const req = request(
+        {
+          hostname: HUMIDIFIER_IP,
+          port: HUMIDIFIER_PORT,
+          path: '/switch/kauf_plug',
+          method: 'GET',
+        },
+        (res) => {
+          let data = ''
+          res.on('data', (chunk) => {
+            data += chunk
+          })
+          res.on('end', () => {
+            const json = JSON.parse(data)
+            this.states.on = json.value
+            resolve(this.states.on)
+          })
+        }
+      )
+      req.on('error', (error) => {
+        reject(error.name + '\n' + error.message + '\n' + error.stack)
+      })
+      req.end()
+    })
+  }
+}
--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,12 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'ExampleHomebridgePlugin';
+export const PLATFORM_NAME = 'ClimateControllerPlugin'
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-plugin-name';
+export const PLUGIN_NAME = 'homebridge-climate-controller'
+
+export const HUMIDIFIER_IP = '192.168.0.64'
+export const HUMIDIFIER_PORT = '80'
